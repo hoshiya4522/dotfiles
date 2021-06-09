5ea6724f09bc7885e3a2b0218e2c9b609b736171
hoshiya <dhrubodev@gmail.com>
2021-06-09 12:54:37 +0600

    My bad

--- justdoit.sh
+++ justdoit.sh
@@ -1,3 +1,6 @@
+echo "Doing Git Add..\n"
 git add .
-git commit -m "$(shuf -n 1 lazy.txt)"
+echo "Commiting..\n"
+git commit -m "$(curl http://whatthecommit.com/index.txt)" || echo "Cant fetch.."; git commit -m "$(shuf -n 1 lazy.txt)"
+echo "Pushing..\n"
 git push -u origin master
