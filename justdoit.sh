078bb758c39863e4ea81506d41c0e5ad577ff7a4
hoshiya <dhrubodev@gmail.com>
2021-06-09 13:01:04 +0600

    just trolling the repo

--- justdoit.sh
+++ justdoit.sh
@@ -1,6 +1,6 @@
 echo "Doing Git Add..\n"
 git add .
 echo "Commiting..\n"
-git commit -m "$(curl --silent http://whatthecommit.com/index.txt)" || echo "Cant fetch.." && git commit -m "$(shuf -n 1 lazy.txt)"
+git commit -m "$(curl --silent http://whatthecommit.com/index.txt)"
 echo "Pushing..\n"
 git push -u origin master
