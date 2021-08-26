dd24340b4be770559c1fa52975e6a4d1332d1ce4
hoshiya4522 <dhrubodev@gmail.com>
2021-08-26 10:43:07 +0600

    now i can fzf into history

--- zsh/.config/zsh/plugins.zsh
+++ zsh/.config/zsh/plugins.zsh
@@ -12,3 +12,7 @@ source ~/.config/plugins/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#696969"
 # Starship
 eval "$(starship init zsh)"
+
+
+
+source ~/.config/plugins/zsh/zsh-fzf-history-search/zsh-fzf-history-search.zsh
