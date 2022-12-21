5f762f661294a8c0f356c2dee47bec9ae40e6f40
Gourob Dev <dhrubodev@gmail.com>
2022-12-22 00:59:44 +0600

    ADD lualine for the 100th time

--- .config/nvim/lua/hoshiya/packer.lua
+++ .config/nvim/lua/hoshiya/packer.lua
@@ -34,8 +34,16 @@ return require('packer').startup(function(use)
     use('jiangmiao/auto-pairs')
     use('mattn/emmet-vim')
 
+    -- StatusLine
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+    require('lualine').setup{
+        options = { theme = 'tokyonight' }
+    }
 
-    -- LSP and stuff
+    -- LSP and stuf
     use {
         'VonHeikemen/lsp-zero.nvim',
         requires = {
@@ -59,3 +67,4 @@ return require('packer').startup(function(use)
     }
 
 end)
+
