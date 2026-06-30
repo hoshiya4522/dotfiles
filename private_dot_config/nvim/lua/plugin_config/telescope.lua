8ffe24aee492e5cfa570b7425093985863bd0fdc
Gourob Dev <64770452+hoshiya4522@users.noreply.github.com>
2026-07-01 01:08:41 +0600

    quick

--- private_dot_config/nvim/lua/plugin_config/telescope.lua
+++ private_dot_config/nvim/lua/plugin_config/telescope.lua
@@ -4,3 +4,4 @@ local tsc = require("telescope.builtin")
 
 vim.keymap.set('n', '<leader>ff', tsc.find_files, {desc="[f]ind [f]iles"})
 vim.keymap.set('n', '<leader>fb', tsc.buffers, {desc="[f]ind [b]uffers"})
+vim.keymap.set('n', '<leader>lg', tsc.buffers, {desc="[l]ive [g]rep"})
