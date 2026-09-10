c1f2b752172d4ca5703b60f6e2805648cef9f5b4
Gourob Dev <64770452+hoshiya4522@users.noreply.github.com>
2026-09-10 10:24:20 +0600

    idk

--- private_dot_config/zsh/ros2-jazzy.zsh
+++ private_dot_config/zsh/ros2-jazzy.zsh
@@ -1,4 +1,13 @@
 source /opt/ros/jazzy/setup.zsh
 
+if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+    export QT_QPA_PLATFORM=wayland
+else
+    export QT_QPA_PLATFORM=xcb
+fi
+
 eval "$(register-python-argcomplete ros2)"
 eval "$(register-python-argcomplete colcon)"
+
+export XCURSOR_SIZE=96
+export XCURSOR_THEME=breeze_cursors
